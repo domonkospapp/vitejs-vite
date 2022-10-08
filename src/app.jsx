abc2382f00bb54afb8c7d674f11e16eb5582fd08
domonkospapp <51707177+domonkospapp@users.noreply.github.com>
2022-10-08 20:46:31 +0200

    1 file modified

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import './app.css';
 export function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState('');
+  const [showLogos, setShowLogos] = useState(true);
 
   const add = () => {
     const name = window.prompt();
@@ -21,14 +22,16 @@ export function App() {
         <button onClick={add}>Change!</button>
         <button onClick={remove}>Remove!</button>
       </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
+      {showLogos && (
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+          </a>
+          <a href="https://preactjs.com" target="_blank">
+            <img src={preactLogo} class="logo preact" alt="Preact logo" />
+          </a>
+        </div>
+      )}
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -38,7 +41,7 @@ export function App() {
           Edit <code>src/app.jsx</code> and save to test HMR
         </p>
       </div>
-      <p class="read-the-docs">
+      <p class="read-the-docs" onMouseOver={() => setShowLogos((sl) => !sl)}>
         Click on the Vite and Preact logos to learn more
       </p>
       <p>Result: {name === '' ? '-' : name}</p>
